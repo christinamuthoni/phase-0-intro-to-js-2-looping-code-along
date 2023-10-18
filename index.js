e6a0c68c91853e52375dcc168220365573c55dec
christine <christine.muriungi@student.moringaschool.com>
2023-10-18 14:00:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+const thankYouMessages = names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+
+return thankYouMessages;
+}
+function countDown(number){
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+      }
+    }
+
